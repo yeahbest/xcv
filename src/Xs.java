b8e525cde554a82f2a3ba921fcd5e5892c6046f1
Pawel <funky_pawel@o2.pl>
2016-11-22 22:20:00 +0100

    Initial commit
    initial commit

---
+++ src/Xs.java
@@ -0,0 +1,11 @@
+/**
+ * Created by Pawl on 11/22/2016.
+ */
+public class Xs {
+
+    public static void main(String []args){
+
+        System.out.printf("%s","some string to display dude");
+
+    }
+}
