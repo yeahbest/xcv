8aeb86fc167b2796fe44032edda405f877edf189
Pawel <funky_pawel@o2.pl>
2016-11-23 21:57:17 +0100

    New message

--- src/Xs.java
+++ src/Xs.java
@@ -1,3 +1,5 @@
+import javax.swing.*;
+
 /**
  * Created by Pawl on 11/22/2016.
  */
@@ -5,9 +7,18 @@ public class Xs {
 
     public static void main(String []args){
 
-        System.out.printf("%s","some string to display dude");
-        //some new project to check out
-        System.out.print("dsdsdsd\n");
+
+        String a =JOptionPane.showInputDialog("Enter num to add");
+        String b = JOptionPane.showInputDialog("Ebter the other num to add");
+
+                int ai = Integer.parseInt(a);
+                int bi = Integer.parseInt(b);
+
+        int result = ai + bi;
+        JOptionPane.showMessageDialog(null,result);
+
+
 
     }
+
 }
